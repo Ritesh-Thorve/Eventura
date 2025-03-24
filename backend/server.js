e645988d0fac71d86ca20504ec82771e7ba40017
Ritesh Thorve <riteshthorve2004@gmail.com>
2025-03-24 18:29:23 +0530

    fixed issue

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,5 @@ app.use('/api/bookings', bookingRoute);
 app.use('/api/admin', adminRoute);
 app.use('/api/messages', messageRoute);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
